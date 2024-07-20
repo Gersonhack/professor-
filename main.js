c5784568bdbf85d8e2dc689e73a23af74f2afb1a
gersonhacker009@gmail.con <gersonhacker009@gmail.con>
2024-07-20 14:22:47 +0200

    subir meus arquivos

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log('matrix!');
+function gh(){
+  alert('rede social indisponível')
+}
+
+function sapp(){
+  window.location.href="https://wa.me//+258845163915"
+}
+// Verifica se o dispositivo é um desktop
+function rr() {
+  window.location.href = "robô.html"
+}
